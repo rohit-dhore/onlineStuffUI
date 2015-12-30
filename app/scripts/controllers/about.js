b233b24b3de1b8201620888cb26a84c71ea7a9b3
rohit-creo <rohit.dhore@creospan.com>
2015-12-30 20:00:04 +0530

    my initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onlineStuffApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the onlineStuffApp
+ */
+angular.module('onlineStuffApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name onlineStuffApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the onlineStuffApp
+ */
+angular.module('onlineStuffApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
